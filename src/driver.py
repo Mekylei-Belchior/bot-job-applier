1226624a2c286c84ac8ef8e5f989d931a2dd7a7d
Mekylei-Belchior <mekylei@msn.com>
2020-08-02 13:29:05 -0300

    Update driver.py
    Code refactoring

--- src/driver.py
+++ src/driver.py
@@ -1,3 +1,4 @@
+""" Module imports """
 from os import listdir, path, mkdir
 from os.path import isdir
 from shutil import rmtree
@@ -5,191 +6,197 @@ from shutil import rmtree
 from zipfile import ZipFile, is_zipfile
 from io import BytesIO
 
-from requests import get		as download
-from bs4 import BeautifulSoup	as bfs
+from requests import get as download
+from bs4 import BeautifulSoup as bfs
 
 
 class DriverPreparation():
-	"""
-	Get ready chrome webdriver to use in Selenium.
+    """
+    Get ready chrome webdriver to use in Selenium.
 
-	Variables:
-		p {str} -- Chrome Webdriver page
-		l {str} -- URL to Google API directory
-		c {str} -- Chrome Web Browser directory path
-		d {str} -- Webdriver path
+    Variables:
+        p {str} -- Chrome Webdriver page
+        l {str} -- URL to Google API directory
+        c {str} -- Chrome Web Browser directory path
+        d {str} -- Webdriver path
 
-	"""
+    """
 
-	p = 'https://chromedriver.chromium.org/'
-	l = 'https://chromedriver.storage.googleapis.com/*/chromedriver_win32.zip'
-	c = 'C:/Program Files (x86)/Google/Chrome/Application'
-	d = './driver/'
+    p = 'https://chromedriver.chromium.org/'
+    l = 'https://chromedriver.storage.googleapis.com/*/chromedriver_win32.zip'
+    c = 'C:/Program Files (x86)/Google/Chrome/Application'
+    d = './driver/'
 
-	def __init__(self, driver=d, chrome=c, link=l, page=p):
-		self.driver = driver
-		self.chrome = chrome
-		self.link = link
-		self.page = page	
-
-
-	def __check_path(self, dPath):
-		"""
-		Returns directory path names or None if nothing is found.
-
-		"""
-
-		if path.exists(dPath):
-			path_names = [d for d in listdir(dPath) if isdir(dPath + '/' + d)]
-			return path_names if len(path_names) > 0 else None
-		else:
-			return None
+    def __init__(self, driver=d, chrome=c, link=l, page=p):
+        self.driver = driver
+        self.chrome = chrome
+        self.link = link
+        self.page = page
 
 
-	def chrome_driver(self):
-		"""
-		Returns the chrome driver path to use.
-		
-		"""
+    def __check_path(self, d_path):
+        """
+        Verify the directory of the path argument.
 
-		dp = self.__check_path(self.driver)
-		
-		confirm = all([not dp is None, len(dp[0]) > 11])
+        """
 
-		if confirm:
-			return self.driver + self.driver_version() + '/chromedriver'
-		else:
-			return None
+        if path.exists(d_path):
+            path_names = [d for d in listdir(d_path) if isdir(d_path + '/' + d)]
 
+            if len(path_names) > 0:
+                return path_names
+            return None
+        return False
 
 
-	def __get_version(self, dPath):
-		"""
-		Returns the version of the object if it is found.
-		
-		"""
+    def chrome_driver(self):
+        """
+        Returns the chrome driver path.
 
-		items = self.__check_path(dPath)
-		
-		if not items is None:
-			for item in items:
-				if item.replace('.', '').isdigit() and len(item) > 11:
-					return item
-				else:
-					return None	
-		else:
-			return None
+        """
+        driver = self.driver_version()
+        return self.driver + driver + '/chromedriver'
 
 
-	def chrome_version(self):
-		"""
-		Check chrome browser version.
-		"""
-		version = self.__get_version(self.chrome)
-		
-		if not version == None:
-			return version
-		else:
-			return 'Chrome version not found.'
+    def driver_version(self):
+        """
+        Check webdriver version.
+        """
+        return self.__get_driver_version(self.driver)
 
 
-	def driver_version(self):
-		"""
-		Check webdriver version.
-		"""
-		version = self.__get_version(self.driver)
-		
-		if not version == None:
-			return version
-		else:
-			return 'Webdriver version not found.'
+    def __get_driver_version(self, d_path):
+        """
+        Returns the driver version.
+
+        """
+
+        items = self.__check_path(d_path)
+
+        if items is False:
+            mkdir(self.driver)
+        elif items is None:
+            return self.__download_file()
+        else:
+            for item in items:
+                if item.replace('.', '').isdigit() and len(item) > 11:
+                    return item
+
+        return self.__download_file()
 
 
-	def __check_update(self):
-		"""
-		Check chrome browser and webdriver version.
-		
-		"""
+    def chrome_version(self):
+        """
+        Check chrome browser version.
+        """
+        version = self.__get_chrome_version(self.chrome)
 
-		driverVersion = self.__get_version(self.driver)
-		chromeVersion = self.__get_version(self.chrome)
-		objects = [driverVersion, chromeVersion]
-		
-		items = []
-
-		if all(objects):
-			for item in objects:
-				items.append(item.replace('.', '').isdigit())
-			
-			if all(items):
-				return True if driverVersion < chromeVersion else False
-			else:
-				return None
-		else:
-			return True
+        if not version is None:
+            return version
+        return 'Chrome version not found.'
 
 
-	def __link_download(self):
-		"""
-		Generates download link for the last chrome webdriver stable release.
-		
-		"""
-		
-		html = download(self.page)
-		html = html.text
+    def __get_chrome_version(self, d_path):
+        """
+        Returns the version of web browser Chrome if it is found.
 
-		soup = bfs(html, 'html.parser')
-		tag = soup.select('#sites-canvas-main-content > table > tbody > tr > td > div > div:nth-child(5) > ul > li:nth-child(1) > a')
-		stable_release = list(tag[0])
-		stable_release = stable_release[0][13:]
+        """
 
-		return self.link.replace('*', stable_release), stable_release
+        items = self.__check_path(d_path)
 
+        if not items:
+            return None
 
-	def update_driver(self):
-		"""
-		This method update chrome webdriver whether webdriver version is lower
-		than chrome version.
-
-		"""
-
-		check = self.__check_update()
-
-		if check:
-			self.__download_file()
+        for item in items:
+            if item.replace('.', '').isdigit() and len(item) > 11:
+                return item
 
 
-	def __download_file(self):
-		"""
-		Download chrome webdriver.
-		"""
+    def update_driver(self):
+        """
+        This method update chrome webdriver if webdriver version is lower
+        than chrome version.
 
-		url, release = self.__link_download()
+        """
+        confirm = self.__check_update()
 
-		content = download(url, stream=True)
-		iszip = is_zipfile(BytesIO(content.content))
+        if confirm:
+            version = self.__download_file()
+            return version
 
-		if iszip:
-			file = ZipFile(BytesIO(content.content))
 
-			try:
-				folders = self.__check_path(self.driver)
+    def __check_update(self):
+        """
+        Check chrome browser and webdriver version.
 
-				# Delete each folder in driver directory
-				if not folders is None and len(folders) > 0:
-					for folder in folders:
-						rmtree(self.driver + folder)
-			except Exception as e:
-				print('Could not delete driver folder.', str(e))
+        """
 
-			try:
-				# Create a new driver directory
-				mkdir(self.driver + release)
-				# Unzip the file
-				with file as f:
-					f.extractall(self.driver + release)
+        driver_version = self.driver_version()
+        chrome_version = self.chrome_version()
+        objects = [driver_version, chrome_version]
 
-			except Exception as e:
-				print('Could not salve the file.', str(e))
-		else:
-			print('Could not find a zip file.')
+        items = []
+
+        if all(objects):
+            for item in objects:
+                items.append(item.replace('.', '').isdigit())
+
+            if all(items):
+                if driver_version < chrome_version:
+                    return True
+                return False
+        return True
+
+
+    def __link_download(self):
+        """
+        Generates download link for the last chrome webdriver stable release.
+
+        """
+
+        html = download(self.page)
+        html = html.text
+
+        soup = bfs(html, 'html.parser')
+        tag = soup.select(
+            '#sites-canvas-main-content>table>tbody>tr>td>div>div:nth-child(5)>ul>li:nth-child(1)>a'
+        )
+
+        stable_release = list(tag[0])
+        stable_release = stable_release[0][13:]
+
+        return self.link.replace('*', stable_release), stable_release
+
+
+    def __download_file(self):
+        """
+        Download chrome webdriver.
+        """
+
+        url, release = self.__link_download()
+
+        content = download(url, stream=True)
+        iszip = is_zipfile(BytesIO(content.content))
+
+        if iszip:
+            file = ZipFile(BytesIO(content.content))
+
+            try:
+                folders = self.__check_path(self.driver)
+                # Delete each folder in driver directory
+                if all([folders is not None, folders is not False]):
+                    for folder in folders:
+                        rmtree(self.driver + folder)
+            except Exception as error:
+                print('Could not delete driver folder.', str(error))
+
+            try:
+                # Create a new driver directory
+                mkdir(self.driver + release)
+                # Unzip the file
+                file.extractall(self.driver + release)
+                return release
+            except Exception as error:
+                print('Could not salve the file.', str(error))
+        else:
+            print('Could not find a zip file.')
